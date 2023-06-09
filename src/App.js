5ade4b2f1be38481a7dd66ceab788039157ff0d3
fajarkusumaa <fajarkusuma46@gmail.com>
2023-06-09 11:14:41 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1 style={{ color: 'white', fontSize: 56, marginTop: 24, marginBottom: 0 }}>Pokedex</h1>
-      <h3 style={{ marginBottom: 48, color: '#EFB416' }}>Find your Pocket Monster !</h3>
+      <h3 style={{ marginBottom: 56, color: '#EFB416' }}>Find your Pocket Monster !</h3>
       <PokemonList />
     </div >
   );
